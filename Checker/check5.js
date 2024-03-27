b55f5a407fbb8c156e1aa8769afa9a727df47aab
Prashant Kumar Singh <prashantsingh090798@gmail.com>
2024-03-27 09:15:33 +0530

    bulk connect POC

---
+++ Checker/check5.js
@@ -0,0 +1,9 @@
+const arr = ["Ram", "Shayam", "Bharat", "Shatrugun"]
+console.log(typeof arr)
+console.log(arr)
+arr.map(element => {
+    console.log(element)
+})
+for (let index = 0; index < arr.length; index++) {
+    console.log(arr[index])
+}
